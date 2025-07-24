d9283250a3c7a7097c3c5a3ad3ad7f80963aa82f
Bogdan Gorelkin <b.gorelkin@yandex.ru>
2025-07-24 12:41:48 +0200

    useAttemptListener

--- src/useAttemptListener.ts
+++ src/useAttemptListener.ts
@@ -1,17 +1,13 @@
 import {
-  AnyAction,
   AsyncThunk,
   Dispatch,
-  ListenerMiddleware,
-  MiddlewareArray,
+  ListenerMiddlewareInstance,
   ThunkDispatch,
-  ThunkMiddleware,
-  Unsubscribe,
-  addListener,
+  UnknownAction
 } from '@reduxjs/toolkit'
+
 import { useEffect, useState } from 'react'
-// import { store } from '../../store'
-import { ToolkitStore } from '@reduxjs/toolkit/dist/configureStore'
+
 // AsyncThunkConfig has been copied from /node_modules/@reduxjs/toolkit/dist/createAsyncThunk.d.ts
 declare type AsyncThunkConfig = {
   state?: unknown
@@ -23,25 +19,11 @@ declare type AsyncThunkConfig = {
   fulfilledMeta?: unknown
   rejectedMeta?: unknown
 }
-
-// type AttemptActionPending<Returned, ThunkArg, ThunkApiConfig extends AsyncThunkConfig> = ReturnType<AsyncThunk<Returned, ThunkArg, ThunkApiConfig>['pending']>;
-// type AttemptActionFulfilled<Returned, ThunkArg, ThunkApiConfig extends AsyncThunkConfig> = ReturnType<AsyncThunk<Returned, ThunkArg, ThunkApiConfig>['fulfilled']>;
-// type AttemptActionRejected<Returned, ThunkArg, ThunkApiConfig extends AsyncThunkConfig> = ReturnType<AsyncThunk<Returned, ThunkArg, ThunkApiConfig>['rejected']>;
-
 interface Props<Returned, ThunkArg, ThunkApiConfig extends AsyncThunkConfig> {
-  store: ToolkitStore<
-    any,
-    AnyAction,
-    MiddlewareArray<
-      [
-        ListenerMiddleware<unknown, ThunkDispatch<unknown, unknown, AnyAction>, unknown>,
-        ThunkMiddleware<any, AnyAction>,
-      ]
-    >
-  >
+  listenerMiddleware: ListenerMiddlewareInstance<unknown, ThunkDispatch<unknown, unknown, UnknownAction>, unknown>
+  
   attempt: AsyncThunk<Returned, ThunkArg, ThunkApiConfig>
   onPending?: (action: any) => void
-  // onFulfilled?: (action: PayloadAction<ReturnType<T>>, listenerApi: ListenerEffectAPI<unknown, ThunkDispatch<unknown, unknown, AnyAction>, unknown>) => void
   onFulfilled?: (action: any, listenerApi: any) => void
   onRejected?: (action: any, listenerApi: any) => void
 }
@@ -52,7 +34,7 @@ export function useAttemptListener<
   ThunkApiConfig extends AsyncThunkConfig,
 >({
   attempt,
-  store,
+  listenerMiddleware,
   onPending,
   onFulfilled,
   onRejected,
@@ -60,44 +42,38 @@ export function useAttemptListener<
   const [pending, setPending] = useState<boolean>(false)
 
   useEffect(() => {
-    const removeAttemptPendingListener: Unsubscribe = store.dispatch(
-      addListener({
-        actionCreator: attempt.pending,
-        effect: (action) => {
-          setPending(true)
-          onPending && onPending(action)
-        },
-      }),
-    )
+    const removePending = listenerMiddleware.startListening({
+      actionCreator: attempt.pending,
+      effect: (action) => {
+        setPending(true)
+        onPending?.(action)
+      },
+    })
 
-    const removeAttemptErrorListener: Unsubscribe = store.dispatch(
-      addListener({
-        actionCreator: attempt.rejected,
-        effect: (action, listenerApi) => {
-          setPending(false)
-          onRejected
-            ? onRejected(action, listenerApi)
-            : console.error(`${attempt.name} has been rejected!`)
-        },
-      }),
-    )
+    const removeRejected = listenerMiddleware.startListening({
+      actionCreator: attempt.rejected,
+      effect: (action, listenerApi) => {
+        setPending(false)
+        onRejected?.(action, listenerApi) ??
+          console.error(`${attempt.typePrefix} was rejected.`)
+      },
+    })
 
-    const removeAttemptSuccessListener: Unsubscribe = store.dispatch(
-      addListener({
-        actionCreator: attempt.fulfilled,
-        effect: (action, listenerApi) => {
-          setPending(false)
-          onFulfilled && onFulfilled(action, listenerApi)
-        },
-      }),
-    )
+    const removeFulfilled = listenerMiddleware.startListening({
+      actionCreator: attempt.fulfilled,
+      effect: (action, listenerApi) => {
+        setPending(false)
+        onFulfilled?.(action, listenerApi)
+      },
+    })
 
     return () => {
-      removeAttemptSuccessListener()
-      removeAttemptErrorListener()
-      removeAttemptPendingListener()
+      removePending()
+      removeRejected()
+      removeFulfilled()
     }
-  }, [attempt])
+  }, [attempt, listenerMiddleware, onPending, onRejected, onFulfilled])
+
 
   return pending
 }
